4387dc53cb0ab4ea6237edabf3e7df6a619384c3
Niek ten Hoopen <niek@nthmedia.nl>
2019-08-31 16:27:35 +0200

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Entry GPS Coordinates plugin for Craft CMS 3.x
+
+Pick a GPS location for an entry
+
+![PluginImpression](https://user-images.githubusercontent.com/3450011/64065265-e3cbc680-cc0b-11e9-89fc-ed682123b109.gif)
+
---
+++ src/translations/en/entry-gps-coordinates.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Entry GPS Coordinates plugin for Craft CMS 3.x
+ *
+ * Pick a GPS location for an entry
+ *
+ * @link      https://nthmedia.nl
+ * @copyright Copyright (c) 2019 NTH media
+ */
+
+/**
+ * Entry GPS Coordinates en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('entry-gps-coordinates', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    NTH media
+ * @package   EntryGpsCoordinates
+ * @since     1.0.0
+ */
+return [
+    'Entry GPS Coordinates plugin loaded' => 'Entry GPS Coordinates plugin loaded',
+];
